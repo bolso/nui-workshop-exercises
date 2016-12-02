93541c3cf305d8a050f26558aa29adc645e100d9
Valery Gorodnichev <ValeryG@concur.com>
2016-12-02 08:56:44 +0300

    removing commentes from pre solution

--- 04/solution/src/service/locations.js
+++ 04/solution/src/service/locations.js
@@ -1,7 +1,5 @@
-// using import instead of require
 import  uuid from 'uuid';
 
-//using map and ...
 let locations = [
     {country: "Germany", city: "Frankfurt", numEmployees: 20},
     {country: "France", city: "Paris", numEmployees: 121},
@@ -11,24 +9,18 @@ let locations = [
     {...l, ...{id: uuid.v4()}}
 ));
 
-//use es6 export and fat arrrow
 export const getLocations = (n) => locations.slice(0, n);
 
-//function that accepts location Object
-//and appends it to locations array and returns new array of locations.
 export const updateLocation = (location) => {
 
-    // setting id on location if missing
     location.id = location.id || uuid.v4();
     const idx = locations.findIndex(l => {
         return l.id == location.id
     })
 
     if (idx === -1) {
-        //append, no mutiation
         locations = [...locations, location];
     } else {
-        //replace, no mutation
         locations = [
             ...locations.slice(0, idx-1),
             ...[location],
