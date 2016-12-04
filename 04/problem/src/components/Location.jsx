99b481acf5f6173a7b9ab1223e31a53de4ae8e90
Valery Gorodnichev <ValeryG@concur.com>
2016-12-04 22:52:19 +0100

    more hints for 04

--- 04/problem/src/components/Location.jsx
+++ 04/problem/src/components/Location.jsx
@@ -1,5 +1,15 @@
+import React  from 'react';
+
 //create react compomnent to render individual locationsList
 //use stateless functional component syntax
 //use conditional rendering of size (big || small) based on numEmployees
 
-export default () ...
+export default ({country, city, numEmployees}) => (
+    <div>
+        {/* render displaying country city and num employees */}
+        ...
+
+        {/* render Size using conditional rendering */}
+         
+    </div>
+);
