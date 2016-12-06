3e56eed5cadd123f5feedd97be9571c83574729a
Valery Gorodnichev <ValeryG@concur.com>
2016-12-06 09:05:55 +0100

    fix in comment

--- 04/problem/src/app.js
+++ 04/problem/src/app.js
@@ -1,4 +1,4 @@
-//import react locationsList component from components/LocationList
+//import react locationsPage component from components/LocationPage
 //and render it to div with id="root" using ReactDOM.render
 import locationsPage from './components/LocationsPage';
 
