580f724acd51dd20544c6f927639b671590947c5
Valery Gorodnichev <ValeryG@concur.com>
2016-12-06 09:11:26 +0100

    less typing in LocationsList.js

--- 04/problem/src/components/LocationsList.js
+++ 04/problem/src/components/LocationsList.js
@@ -1,2 +1,11 @@
+import React  from 'react';
+import Location from './Location';
 
-export default ({locations}) ...
+export default ({locations}) => (
+    <div>
+        <ul>
+        {/* loop trough locations listt and render each location in <li> */}
+
+        </ul>
+    </div>
+)
